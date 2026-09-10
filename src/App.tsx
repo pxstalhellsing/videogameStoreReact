84999fb35453375c1b4e74b8e7bef108f33cc941
pxstalhellsing <akagamivergilx655@gmail.com>
2026-09-10 18:18:42 -0300

    Prueba 1

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
+          <p>Hola</p>
         </div>
         <div>
           <h1>Get started</h1>
