91e1861deec77f16af481cee8294d058fa60aca9
pxstalhellsing <akagamivergilx655@gmail.com>
2026-09-14 23:25:08 -0300

    agregado de selectores

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
 import { useState } from 'react'
+import Navbar from './Componentes/Navbar'
 
 import './App.css'
 
 function App() {
   const [vista, setvista] = useState("catalogo");
   const [carro, setcarro] = useState([]);
+  const [favoritos, setfavoritos] = useState([])
 
   return (
     <>
       <Navbar
         setvista={setvista}
-        ConteoCarro={carro.leght}
+        cartCount={carro.length}
+        favCount={favoritos.length}
       />
+      {vista === "catalogo" && (
+
+      <div>
+        <h1>Catálogo de videojuegos</h1>
+      </div>
+
+      )}
+
+      {vista === "favoritos" && (
+
+      <div>
+        <h1>Mis favoritos</h1>
+      </div>
+
+
+
+
+
+      )}
+
+      {vista === "carrito" && (
+
+      <div>
+        <h1>Mi carrito</h1>
+      </div>
+
+      )}
+      
     </>
   );
 }
