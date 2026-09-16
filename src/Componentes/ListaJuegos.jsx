eed8bdbb22075a6d6b1ccac36c338037bf04d7a6
pxstalhellsing <akagamivergilx655@gmail.com>
2026-09-15 22:10:07 -0300

    leve modificacion y detalle agregado

--- src/Componentes/ListaJuegos.jsx
+++ src/Componentes/ListaJuegos.jsx
@@ -1,16 +1,35 @@
 import TarjetaJuegos from './TarjetaJuegos';
 
-const ListaJuegos = ({ juegos, agregarAlCarrito, alternarFavorito, favoritos }) => {
+const ListaJuegos = ({ 
+  juegos, 
+  agregarAlCarrito, 
+  alternarFavorito, 
+  favoritos 
+}) => {
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
-      {juegos.map((juego) => (
-         <TarjetaJuegos 
-            key={juego.id}
-            juego={juego}
-            agregarAlCarrito={agregarAlCarrito}
-            alternarFavorito={alternarFavorito}
-            esFavorito={favoritos.some(fav => fav.id === juego.id)}
+      {juegos.map((game) => (
+
+       <div 
+        className="col-12 col-sm-6 col-md-4 col-lg-3"
+        key={game.id}
+      > 
+
+         <TarjetaJuegos
+            game={game}
+            agregarAlCarrito={agregarCarrito}
+            toggleFavorite={toggleFavorite}
+            esFavorito={
+              favoritos.some(
+                (favoritos) => 
+                  favorito.id === game.id
+                  
+                )
+              }
+            verDetalle={verDetalle}
          />
+        </div>
       ))}
     </div>
   );
